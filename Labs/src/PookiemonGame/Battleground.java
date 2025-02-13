caec66ab52e9b8a1c71958411ee73ce28214604e
Mitchell Peck <50884550+MitchellPeck@users.noreply.github.com>
2025-02-12 20:23:47 -0500

    wee

--- Labs/src/PookiemonGame/Battleground.java
+++ Labs/src/PookiemonGame/Battleground.java
@@ -9,73 +9,60 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Battleground {
-    private ArrayList<Player> players;
     private Player player;
     private Players playersList;
     Scanner i = new Scanner(System.in);
     PookiemonList pList = new PookiemonList();
 
-    public Battleground() {
-        players = new ArrayList<Player>();
-    }
-
-    private void addPlayer(Player player) {
-        players.add(player);
-        if (players.size() == 1 || player == null) player = players.getFirst();
-    }
-
-    private void removePlayer(Player player) {
-        players.remove(player);
-        if (players.isEmpty()) player = null;
-    }
-
-    private void removePlayer(int index) {
-        players.remove(index);
-        if (players.isEmpty()) player = null;
-    }
+    public Battleground() {}
 
     private void setPlayer(int index) {
-        player = players.get(index);
+        player = playersList.getPlayer(index);
     }
 
     private void nextPlayer() {
-        if (players.size() < 2) return;
-        if (player == players.getLast()) player = players.getFirst();
-        else player = players.get(players.indexOf(player) + 1);
+        if (playersList.getSize() < 2) return;
+        if (player == playersList.getList().getLast()) player = playersList.getList().getFirst();
+        else player = playersList.getPlayer(playersList.getList().indexOf(player) + 1);
     }
 
     private Player getOpponent() {
-        if (players.size() < 2) return null;
-        if (player == players.getLast()) return players.getFirst();
-        else return players.get(players.indexOf(player) + 1);
+        if (playersList.getSize() < 2) return null;
+        if (player == playersList.getList().getLast()) return playersList.getList().getFirst();
+        else return playersList.getPlayer(playersList.getList().indexOf(player) + 1);
     }
 
     public void play() {
         System.out.println("Welcome to Pookiemon!");
 
+        Player[] playerList;
+
         int response = Utils.getValidEntry("Would you like to play (1) by yourself against a computer or (2-4) with a friend(s)?", 1, i, 1, 4);
         if (response == 1) {
+            playerList = new Player[2];
             System.out.println("Lonely!");
             Player player1 = Utils.createPlayer(i);
-            players.add(player1);
+            playerList[0] = player1;
             player = player1;
             Pookiemon[] list = Utils.randomList();
             Player player2 = new Player("Computer", false, list, Utils.randomPookiemon(list));
-            players.add(player2);
+            playerList[1] = player2;
         } else {
+            playerList = new Player[response];
             for (int i=0; i<response; i++) {
                 System.out.println("Player " + (i+1) + ", let's create your player.");
                 Player player = Utils.createPlayer(this.i);
-                players.add(player);
+                playerList[i] = player;
                 if (i == 0) this.player = player;
             }
         }
-        playersList = new Players(players);
+        playersList = new Players(playerList);
 
         System.out.println("Let's play!");
 
         while (playersList.winningPlayer() == null) {
             System.out.println(player.getName() + ", it's your turn!");
+            System.out.println("You'll be fighting against " + getOpponent().getName());
         }
     }
 }
